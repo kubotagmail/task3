8a6f9213cb0c5df1c8802f242d89792bfefb1132
EC2 Default User <ec2-user@ip-172-31-37-21.ap-northeast-1.compute.internal>
2022-09-20 04:29:56 +0000

    Initial Commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,10 +18,10 @@ class User < ApplicationRecord
     
 # もし写真を持っていなかったら、unless、持ってたらその写真を表示
 def get_profile_image(width, height)
-  # unless image.attached?
-  #   file_path = Rails.root.join('app/assets/images/no_image.jpg')
-  #   image.attach(io: File.open(file_path), filename: 'no_image.jpg', content_type: 'image/jpg')
-  # end
+   unless image.attached?
+    file_path = Rails.root.join('app/assets/images/no_image.jpg')
+     image.attach(io: File.open(file_path), filename: 'no_image.jpg', content_type: 'image/jpg')
+   end
     image.variant(resize_to_limit: [width, height]).processed
 end
 
